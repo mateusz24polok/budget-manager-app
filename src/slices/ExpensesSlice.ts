1dbbd4280fcf8056626c1597567b968d6ef723eb
Mateusz Polok <mateusz24polok@gmail.com>
2020-12-02 18:33:32 +0100

    Typing has been introduced into the ExpensesSlice and removeExpense functionality has been added into the app

--- src/slices/ExpensesSlice.ts
+++ src/slices/ExpensesSlice.ts
@@ -1,21 +1,31 @@
-import { RootRef } from "@material-ui/core";
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { exampleData } from "../exampleData";
 import { RootState } from "../store";
+import { SingleExpense } from "../interfaces";
+
+type ExpensesSliceState = {
+  expenses: Array<SingleExpense>;
+  isAddExpenseModalOpen: boolean;
+  isEditExpenseModalOpen: boolean;
+};
+
+const initialState: ExpensesSliceState = {
+  expenses: exampleData,
+  isAddExpenseModalOpen: false,
+  isEditExpenseModalOpen: false,
+};
 
 const ExpensesSlice = createSlice({
   name: "expenses",
-  initialState: {
-    expenses: exampleData,
-    isAddExpenseModalOpen: false,
-    isEditExpenseModalOpen: false,
-  },
+  initialState,
   reducers: {
     addExpense: () => {},
 
     editExpense: () => {},
 
-    removeExpense: () => {},
+    removeExpense: (state, action: PayloadAction<number>) => {
+      state.expenses = state.expenses.filter((expense) => expense.id !== action.payload);
+    },
 
     openAddExpenseModal: (state) => {
       state.isAddExpenseModalOpen = true;
@@ -36,12 +46,9 @@ const ExpensesSlice = createSlice({
 });
 
 export const selectExpensesState = (state: RootState) => state.expenses;
-export const selectExpenses = (state: RootState) =>
-  selectExpensesState(state).expenses;
-export const selectisAddExpenseModalOpen = (state: RootState) =>
-  selectExpensesState(state).isAddExpenseModalOpen;
-export const selectisEditExpenseModalOpen = (state: RootState) =>
-  selectExpensesState(state).isEditExpenseModalOpen;
+export const selectExpenses = (state: RootState) => selectExpensesState(state).expenses;
+export const selectisAddExpenseModalOpen = (state: RootState) => selectExpensesState(state).isAddExpenseModalOpen;
+export const selectisEditExpenseModalOpen = (state: RootState) => selectExpensesState(state).isEditExpenseModalOpen;
 
 export const {
   addExpense,
