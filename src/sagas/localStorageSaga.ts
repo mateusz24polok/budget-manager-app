7523562b9b6cfc8503880f131b4fe4efc9a115e4
Mateusz Polok <mateusz24polok@gmail.com>
2020-12-05 19:14:40 +0100

    Minor change in formatting of one script has been done

--- src/sagas/localStorageSaga.ts
+++ src/sagas/localStorageSaga.ts
@@ -5,13 +5,12 @@ import { SingleExpenseInterface } from "../interfaces";
 function* localStorageActionHandler() {
   const expenses: Array<SingleExpenseInterface> = yield select(selectExpenses);
   yield localStorage.setItem("Expenses", JSON.stringify(expenses))
-}
+};
 
 export function* localStorageSaga() {
     yield all([
         takeLatest(addExpense.type, localStorageActionHandler),
         takeLatest(editExpense.type, localStorageActionHandler),
         takeLatest(removeExpense.type, localStorageActionHandler)
-    ])
-;
-}
+    ]);
+};
